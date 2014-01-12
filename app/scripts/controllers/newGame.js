3f888f9abaede848666e30153c41fa9632569cfc
Dennis Roethig <roethig.dennis@gmail.com>
2014-01-12 17:58:09 +1100

    New Game Controller documented

--- app/scripts/controllers/newGame.js
+++ app/scripts/controllers/newGame.js
@@ -11,31 +11,43 @@ angular.module('totalrecallApp')
 
     .controller('NewGameCtrl', function ($scope, $rootScope, $location, TotalRecallApi, GameInfo) {
 
+        // SCOPE VARIABLES
         $scope.userName = 'dennis';
         $scope.userEmail = 'roethig.dennis@gmail.com';
 
+
+        // LISTEN FOR 'NEW GAME' CLICK
         $scope.startGame = function () {
 
+            // Get name & email from templates
             var name = $scope.userName,
                 email = $scope.userEmail,
                 request;
 
-            $rootScope.$broadcast('overlay:show', {
-                title: 'Loading...'
-            });
-
+            // name && email is filled out, request new game
             if (name && email) {
 
+                // Trigger Overlay with 'Loading...'
+                $rootScope.$broadcast('overlay:show', {
+                    title: 'Loading...'
+                });
+
+                // API class
                 request = TotalRecallApi.startGame(name, email);
 
+                // Promise
                 request.then(function (response) {
 
+                    // Assign game data to provider object
                     GameInfo.data = {
                         gameId: response.gameId,
                         cards: response.cards
                     };
 
+                    // Redirect to play page
                     $location.path('/play');
+
+                    // Trigger 'overlay:hide' event
                     $rootScope.$broadcast('overlay:hide', {});
 
                 });
